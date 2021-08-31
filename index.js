2bbfad23a57886fbec0ebc0a17aa3ecb146762eb
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2021-08-30 23:52:00 -0300

    add dotenv

--- index.js
+++ index.js
@@ -1,94 +1,112 @@
-const express = require('express');
-const gameSchema = require('./models/game');
+const express = require("express");
+const gameSchema = require("./models/game");
 const mongoose = require("./database");
-const { exists } = require('./models/game');
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
 app.use(express.json());
+
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+
+const port = process.env.PORT || 3000;
+const connectionString = `mongodb://${dbHost}:${dbPort}/${dbName}`;
 
 
 // GET - Home
 app.get("/", (req, res) => {
-    res.send({ info: "Hello, guys!"});
-})
-
+  res.send({ info: "Hello, guys!" });
+});
 
 // GET /games - GET ALL GAMES
-app.get('/games', async (req, res) => {
-    const games = await gameSchema.find();
-    res.send({games});
-})
+app.get("/games", async (req, res) => {
+  const games = await gameSchema.find();
+  res.send({ games });
+});
 
-// GET /games/:id - get a movie ById
-app.get('/games/:id', async (req, res) => {
-    const id = req.params.id;
+// GET /games/:id - get a movie BY ID
+app.get("/games/:id", async (req, res) => {
+  const id = req.params.id;
 
-    if( !mongoose.Types.ObjectId.isValid(id) ) {
-        res.status(422).send({error: 'Invalid ID'})
-        return;
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(422).send({ error: "Invalid ID" });
+    return;
+  }
 
-    const game = await gameSchema.findById(id);
+  const game = await gameSchema.findById(id);
 
-    if(!game){
-        res.status(404).send({error: 'Game not found'})
-        return;
-    }
-    res.send({game});
-})
+  if (!game) {
+    res.status(404).send({ error: "Game not found" });
+    return;
+  }
 
-
+  res.send({ game });
+});
 
 // POST - Create a new game
-app.post('/games', async (req, res) => {
-    const game = req.body;
+app.post("/games", async (req, res) => {
+  const game = req.body;
 
-    if(!game || !game.name || !game.genre){
-        res.status(400).send({error: 'Invalid game.'});
-        return;
-    }
+  if (!game || !game.name || !game.genre) {
+    res.status(400).send({ error: "Invalid game." });
+    return;
+  }
 
-    const newGame = await new gameSchema(game).save();
+  const newGame = await new gameSchema(game).save();
 
-    res.status(201).send({newGame})
-})
-
+  res.status(201).send({ info: "Succesfully created the game." });
+});
 
 // PUT - /games/:id - UPDATE a game
-app.put('/games/:id', async (req, res) => {
-    const id = req.params.id;
+app.put("/games/:id", async (req, res) => {
+  const id = req.params.id;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(422).send({ error: "Invalid ID" });
+    return;
+  }
 
-    if( !mongoose.Types.ObjectId.isValid(id) ) {
-        res.status(422).send({error: 'Invalid ID'})
-        return;
-    }
+  const game = await gameSchema.findById(id);
 
-    const game = await gameSchema.findById(id);
+  if (!game) {
+    res.status(404).send({ error: "Game not found" });
+    return;
+  }
 
-    if(!game){
-        res.status(404).send({error: 'Game not found'})
-        return;
-    }
+  const newGame = req.body;
 
-    const newGame = req.body;
+  if (!game || !game.name || !game.genre) {
+    res.status(400).send({ error: "Invalid game." });
+    return;
+  }
 
-    if(!game || !game.name || !game.genre){
-        res.status(400).send({error: 'Invalid game.'});
-        return;
-    }
+  await gameSchema.findOneAndUpdate({ _id: id }, newGame);
+  const updateGame = await gameSchema.findById(id);
 
-    await gameSchema.findOneAndUpdate({_id: id}, newGame);
-    const updateGame = await gameSchema.findById(id);
+  res.send({ info: "Succesfully updated the game." });
+});
 
-    res.send({updateGame})
-    
-})
+// DELETE - /games/:id
+app.delete("/games/:id", async (req, res) => {
+  const id = req.params.id;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(422).send({ error: "Invalid ID" });
+    return;
+  }
 
+  const game = await gameSchema.findById(id);
 
+  if (!game) {
+    res.status(404).send({ error: "Game not found" });
+    return;
+  }
 
-
+  await gameSchema.findByIdAndRemove(id);
+  res.send({ message: "Succesfully removed the game." });
+});
 
 app.listen(port, () => {
-    console.log(`Rodando o server em: http://localhost:${port}`)
-})
+  console.log(`Rodando o server em: http://${dbHost}:${port}`);
+});
